9cfb9a5af7f790db264b8ec384fa320a09f68e6c
dgrayg <159087346+dgrayg@users.noreply.github.com>
2024-05-16 13:28:29 -0600

    Update HashSet.java

--- HashSet.java
+++ HashSet.java
@@ -1,50 +1,123 @@
+import java.util.Arrays;
+import java.util.TreeSet;
+
+
 public class HashSet {
 
-    private int[] values;
-    // FIXME more member variables here
 
-    public HashSet(int initArrayLength, int ratio, ProbeStrategy strategy) {
-        // FIXME to be written
+    private int[] values;
+    private int size; 
+    private final double loadFactor; 
+    private final ProbeStrategy strategy; 
+
+    public HashSet(int initArrayLength, double ratio, ProbeStrategy strategy) {
+        this.values = new int[initArrayLength];
+        this.size = 0;
+        this.loadFactor = ratio;
+        this.strategy = strategy;
+        Arrays.fill(values, -1);
     }
 
     public boolean add(int value) {
-        // FIXME to be written
-        return false; // FIXME remove when ready
+        if (shouldResize()) {
+            resize();
+        }
+
+        int index = value % values.length;
+
+        while (values[index] != -1) {
+            if (values[index] == value) {
+                return false; 
+            }
+            index = strategy.probe(values.length, index, size);
+        }
+
+        values[index] = value;
+        size++;
+        return true;
     }
 
     public boolean contains(int value) {
-        // FIXME to be written
-        return false; // FIXME remove when ready
+        int index = value % values.length;
+
+        while (values[index] != -1) {
+            if (values[index] == value) {
+                return true; 
+            }
+            index = strategy.probe(values.length, index, size);
+        }
+
+        return false;
     }
 
     public boolean remove(int value) {
-        // FIXME to be written
-        return false; // FIXME remove when ready
+        int index = value % values.length;
+
+        while (values[index] != -1) {
+            if (values[index] == value) {
+                values[index] = -1;
+                size--;
+
+                int nextIndex = strategy.probe(values.length, index, size);
+                while (values[nextIndex] != -1) {
+                    int temp = values[nextIndex];
+                    values[nextIndex] = -1;
+                    add(temp); 
+                    nextIndex = strategy.probe(values.length, nextIndex, size);
+                }
+                return true;
+            }
+            index = strategy.probe(values.length, index, size);
+        }
+
+        return false;
+    }
+
+    private boolean shouldResize() {
+        return size >= loadFactor * values.length;
+    }
+
+    private void resize() {
+        int newSize = 2 * values.length + 1;
+        int[] temp = new int[newSize];
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != -1) {
+                add(values[i]); // Re-insert using add to handle collisions
+            }
+        }
+        values = temp;
     }
 
     public int[] toArray() {
-        int[] result = new int[this.values.length];
-        for (int i = 0; i < this.values.length; i++) {
-            result[i] = this.values[i];
+        int[] result = new int[size];
+        int count = 0;
+        for (int i = 0; i < values.length; i++) {
+            if (values[i] != -1) { 
+                result[count++] = values[i]; 
+            }
         }
         return result;
     }
 
+    public int size() {
+        return size;
+    }
+
     public static void main(String[] args) {
-        ProbeStrategy linear = new SimpleLinearProbe();
+        ProbeStrategy linear = new LinearProbeStrategy();
         HashSet set = new HashSet(7, 3, linear);
 
         int[] numbers = {20, 5, 3, 9};
         for (int i = 0; i < numbers.length; i++) {
-            // add the number
             set.add(numbers[i]);
 
-            // print out the array
             int[] array = set.toArray();
             for (int j = 0; j < array.length; j++) {
                 System.out.print(array[j] + ", ");
             }
             System.out.println();
+
         }
+
     }
 }
