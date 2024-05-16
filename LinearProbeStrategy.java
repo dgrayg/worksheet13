d6195c9a9c05f67f930f58f31107431767dc993f
dgrayg <159087346+dgrayg@users.noreply.github.com>
2024-05-16 13:29:23 -0600

    Update LinearProbeStrategy.java

--- LinearProbeStrategy.java
+++ LinearProbeStrategy.java
@@ -1,8 +1,6 @@
 public class LinearProbeStrategy implements ProbeStrategy {
 
-    public int probe(int arraySize, int originalHash, int attemptNum) {
-	// FIXME to be written
-	return 0; // FIXME remove when ready
-    }
-
+  public int probe(int arraySize, int originalHash, int attemptNum) {
+    return (originalHash + attemptNum) % arraySize;
+  }
 }
