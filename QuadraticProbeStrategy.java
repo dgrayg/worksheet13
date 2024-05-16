248dc53105fc53020fc06daabba230e94c2d7c8b
dgrayg <159087346+dgrayg@users.noreply.github.com>
2024-05-16 13:30:17 -0600

    Update QuadraticProbeStrategy.java

--- QuadraticProbeStrategy.java
+++ QuadraticProbeStrategy.java
@@ -1,8 +1,7 @@
 public class QuadraticProbeStrategy implements ProbeStrategy {
 
-    public int probe(int arraySize, int originalHash, int attemptNum) {
-	// FIXME to be written
-	return 0; // FIXME remove when ready
-    }
-
+  public int probe(int arraySize, int originalHash, int attemptNum) {
+    // Quadratic probing formula: (originalHash + attemptNum^2) % arraySize
+    return (originalHash + attemptNum * attemptNum) % arraySize;
+  }
 }
